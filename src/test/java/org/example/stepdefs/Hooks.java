f36215de1d424d1ebfe68a7a2eb1a90af89d9e65
asus <hamedashraf650@gmail.com>
2024-03-17 10:16:52 +0200

    add comments to Hooks

--- src/test/java/org/example/stepdefs/Hooks.java
+++ src/test/java/org/example/stepdefs/Hooks.java
@@ -9,23 +9,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Hooks {
-    public static WebDriver driver ;
+    // Declare a static WebDriver object to be used across test scenarios
+    public static WebDriver driver;
+
+    // This method runs before each scenario and sets up the WebDriver instance
     @Before
-    public  void OpenBrowser(){
-
+    public void openBrowser() {
+        // Setup ChromeDriver using WebDriverManager
         WebDriverManager.chromedriver().setup();
+
+        // Initialize ChromeDriver
         driver = new ChromeDriver();
+
+        // Maximize the browser window
         driver.manage().window().maximize();
+
+        // Set implicit wait timeout to 7 seconds to handle page loading and element location
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+
+        // Navigate to the Bing website
         driver.navigate().to("https://www.bing.com");
-
-
-
     }
 
+    // This method runs after each scenario and closes the WebDriver instance
     @After
-    public void QuitDriver() throws InterruptedException {
+    public void quitDriver() throws InterruptedException {
+        // Add a delay of 5 seconds to ensure all actions are completed before quitting the browser
         Thread.sleep(5000);
+
+        // Quit the WebDriver instance
         driver.quit();
     }
 }
